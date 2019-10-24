8d325a8ae399ecbdf5b330b9dbdb6372e1b8e3f8
Jan <jan.binder0904@gmail.com>
2019-10-24 16:57:07 +0200

    Initial Project

---
+++ game-trainee-web/src/main/java/org/game/trainee/rest/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package org.game.trainee.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
