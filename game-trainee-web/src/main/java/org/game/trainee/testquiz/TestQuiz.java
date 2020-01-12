5d085ae65ae631e4bf0f30c225cdfef2b6fe2716
Eric <eric.haneder@untergrub.at>
2020-01-12 22:43:22 +0100

    Taking a quiz now works properly, started woring on the results-page, added Courses to the courses&quizzes page

--- game-trainee-web/src/main/java/org/game/trainee/testquiz/TestQuiz.java
+++ game-trainee-web/src/main/java/org/game/trainee/testquiz/TestQuiz.java
@@ -1,24 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.game.trainee.testquiz;
+
+import java.io.Serializable;
 
 /**
  *
  * @author Eric
  */
-public class TestQuiz {
+public class TestQuiz implements Serializable {
         
    public String frage;
    public String[] antworten;
    public int indexrichtig;
+   public Boolean[] buttons;
    
    public TestQuiz(String frage, String[] antworten, int indexrichtig) {
       this.frage=frage;
       this.antworten=antworten;
       this.indexrichtig=indexrichtig;
+      buttons = new Boolean[2];
    }
    
     public String getFrage() {
@@ -43,6 +42,14 @@ public class TestQuiz {
 
     public void setIndexrichtig(int indexrichtig) {
         this.indexrichtig = indexrichtig;
+    }
+
+    public Boolean[] getButtons() {
+        return buttons;
+    }
+
+    public void setButtons(Boolean[] buttons) {
+        this.buttons = buttons;
     }
 
 }
--- game-trainee-web/src/main/java/org/game/trainee/testquiz/TestQuizAnzeigen.java
+++ game-trainee-web/src/main/java/org/game/trainee/testquiz/TestQuizAnzeigen.java
@@ -6,6 +6,7 @@
 package org.game.trainee.testquiz;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 //import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -20,15 +21,12 @@ import org.game.trainee.traineeview.TraineeGenerator;
  * @author Eric
  */
 @Named
-@RequestScoped
+@ApplicationScoped
 public class TestQuizAnzeigen implements Serializable {
      
     private List<TestQuiz> quiz;
-     
-    private TestQuiz selectedQuestion;
-    private String selectedAnswer;
-    private Boolean[] buttons = new Boolean[2];
-    private double score; //Der Score sollte später injected werden, und mit einer DB verbunden sein
+    private List<String> results;
+    private int score; //Der Score sollte später injected werden, und mit einer DB verbunden sein
     
     @Inject 
     private TraineeGenerator trainee;
@@ -39,8 +37,8 @@ public class TestQuizAnzeigen implements Serializable {
     @PostConstruct
     public void init() {
         quiz = speicher.createQuiz(4);
-        score = trainee.getProgressFromIndex(0);
-        //buttons = speicher.createButtons(2);
+        score = (int)trainee.getProgressFromIndex(0);
+        results = new ArrayList<>();
     }
  
     public List<TestQuiz> getQuiz() {
@@ -50,68 +48,49 @@ public class TestQuizAnzeigen implements Serializable {
     public void setSpeicher(TestQuizSpeicher speicher) {
         this.speicher = speicher;
     }
- 
-    public TestQuiz getSelectedQuestion() {
-        return selectedQuestion;
-    }
- 
-    public void setSelectedQuestion(TestQuiz selectedQuestion) {
-        this.selectedQuestion = selectedQuestion;
-    }
 
-    public String getSelectedAnswer() {
-        return selectedAnswer;
-    }
-
-    public void setSelectedAnswer(String selectedAnswer) {
-        this.selectedAnswer = selectedAnswer;
-    }
-
-    public Boolean[] getButtons() {
-        return buttons;
-    }
-
-    public void setButtons(Boolean[] buttons) {
-        this.buttons = buttons;
-    }
-
-    public double getScore() {
+    public int getScore() {
         return score;
     }
 
-    public void setScore(double score) {
+    public void setScore(int score) {
         this.score = score;
+    }
+
+    public List<String> getResults() {
+        return results;
+    }
+
+    public void setResults(List<String> results) {
+        this.results = results;
     }
     
     public String checkAnswers() {
         evaluateScore();
-        checkForDoubleChecked();
+        //checkForDoubleChecked();
         return "result.xhtml";
-        
     }
     
     public void evaluateScore() {
-       if(buttons[quiz.get(0).indexrichtig])
-           score+=100;
-       if(buttons[quiz.get(1).indexrichtig])
-           score+=1000;
-       if(buttons[quiz.get(2).indexrichtig])
-           score+=10000;
-       if(buttons[quiz.get(3).indexrichtig])
-           score+=100000;
-       //Hier sollte dann noch der Score der Trainees geupdated werden
+        if(quiz.get(0).buttons[0] && quiz.get(0).indexrichtig == 0 || quiz.get(0).buttons[1] && quiz.get(0).indexrichtig == 1) 
+            score+=100;
+        if(quiz.get(1).buttons[0] && quiz.get(1).indexrichtig == 0 || quiz.get(1).buttons[1] && quiz.get(1).indexrichtig == 1) 
+            score+=1000;
+        if(quiz.get(2).buttons[0] && quiz.get(2).indexrichtig == 0 || quiz.get(2).buttons[1] && quiz.get(2).indexrichtig == 1) 
+            score+=10000;
+        if(quiz.get(3).buttons[0] && quiz.get(3).indexrichtig == 0 || quiz.get(3).buttons[1] && quiz.get(3).indexrichtig == 1) 
+            score+=100000;
+        //Hier sollte dann noch der Score der Trainees geupdated werden
+        if(score==111150) {
+            results.add("Du hast alle Fragen richtig beantwortet!");
+            results.add("Du erhälst 80/80 Punkten!");
+        }
     }
     
-    public String checkForDoubleChecked() {
+    /*public String checkForDoubleChecked() {
         if(buttons[0].booleanValue() == buttons[1].booleanValue())
             return "index.xhtml"; //Hier sollte man eine Error-Message einbauen
         return "result.xhtml";
-        
-    }
-    public String checkTest() {
-        if(selectedAnswer.compareToIgnoreCase(quiz.get(0).antworten[0]) == 0) {
-            return "result.xhtml";
-        }
-        return "leaderboard.xhtml";
-    }
+    } */
+   
 }
