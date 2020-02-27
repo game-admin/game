bcf5cce8efb70a50f1252932dbfcb41380ec9d29
Jan <jan.binder0904@gmail.com>
2020-02-27 21:13:11 +0100

    insertSQL fixed

--- game-trainee-web/src/main/resources/META-INF/insertSQL.sql
+++ game-trainee-web/src/main/resources/META-INF/insertSQL.sql
@@ -9,50 +9,50 @@ INSERT INTO game.quiz ("qid", "titel", "beschreibung", "reward") VALUES ('2', 'O
 
 
 
-<<<<<<< HEAD
-INSERT INTO game.frage ("fnr", "qid", "frage", "punktezahl") VALUES ('1', '1', 'What are advantages of Java?', 10);
-INSERT INTO game.frage ("fnr", "qid", "frage", "punktezahl") VALUES ('2', '1', 'What is written after a line of code?', 10);
-INSERT INTO game.frage ("fnr", "qid", "frage", "punktezahl") VALUES ('3', '1', 'What does Object Oriented mean?', 10);
-INSERT INTO game.frage ("fnr", "qid", "frage", "punktezahl") VALUES ('4', '1', 'Which Java Version exist', 10);
-INSERT INTO game.frage ("fnr", "qid", "frage", "punktezahl") VALUES ('5', '2', 'What concept of the following is Java supporting?', 10);
-INSERT INTO game.frage ("fnr", "qid", "frage", "punktezahl") VALUES ('6', '2', 'Which things do objects include?', 10);
-INSERT INTO game.frage ("fnr", "qid", "frage", "punktezahl") VALUES ('7', '2', 'What is not a variabletype which classes can contain?', 10);
-INSERT INTO game.frage ("fnr", "qid", "frage", "punktezahl") VALUES ('8', '2', 'What is the main rule of a constructor?', 10);
+
+INSERT INTO game.frage ("fid", "qid", "frage", "punktezahl") VALUES ('1', '1', 'What are advantages of Java?', 10);
+INSERT INTO game.frage ("fid", "qid", "frage", "punktezahl") VALUES ('2', '1', 'What is written after a line of code?', 10);
+INSERT INTO game.frage ("fid", "qid", "frage", "punktezahl") VALUES ('3', '1', 'What does Object Oriented mean?', 10);
+INSERT INTO game.frage ("fid", "qid", "frage", "punktezahl") VALUES ('4', '1', 'Which Java Version exist', 10);
+INSERT INTO game.frage ("fid", "qid", "frage", "punktezahl") VALUES ('5', '2', 'What concept of the following is Java supporting?', 10);
+INSERT INTO game.frage ("fid", "qid", "frage", "punktezahl") VALUES ('6', '2', 'Which things do objects include?', 10);
+INSERT INTO game.frage ("fid", "qid", "frage", "punktezahl") VALUES ('7', '2', 'What is not a variabletype which classes can contain?', 10);
+INSERT INTO game.frage ("fid", "qid", "frage", "punktezahl") VALUES ('8', '2', 'What is the main rule of a constructor?', 10);
 
 
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('1', '1', 'Flawless', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('2', '1', 'Platform Independent', true);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('3', '1', 'Only compatible with Windows', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('4', '1', 'only compatible with Linux', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('5', '2', '"."', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('6', '2', '"{ or } "', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('7', '2', '";"', true);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('8','2', 'Java RE', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('9', '3', 'Object is a addon for Java', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('10', '3', 'You have to use the program Object to run Java.', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('11', '3', 'Java can be extended with Objects.', true);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('12', '3', 'Java has no Objects', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('13', '4', 'Java S', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('14', '4', 'Java E', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('15', '4', 'Java SS', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('16', '4', 'Java SE', true);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('17', '5', 'Multiple Inheritance', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('18', '5', 'Classes&Methods', true);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('19', '5', 'Functions', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('20', '5', 'None of the above', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('21', '6', 'Classes', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('22', '6', 'only state', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('23', '6', 'only behaviour', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('24', '6', 'state and behaviour', true);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('25', '7', 'Global', true);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('26', '7', 'Local', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('27', '7', 'Class', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('28', '7', 'Instance', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('29', '8', 'Different name as Class', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('30', '8', 'Same name as attribute', false);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('31', '8', 'Same name as Class', true);
-INSERT INTO game.antwortmoeglichkeiten ("antwid", "fnr", "antwort", "richtigeantwort") VALUES ('32', '8', 'Must contain attributes', false);
-=======
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('1', '1', 'Flawless', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('2', '1', 'Platform Independent', true);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('3', '1', 'Only compatible with Windows', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('4', '1', 'only compatible with Linux', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('5', '2', '"."', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('6', '2', '"{ or } "', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('7', '2', '";"', true);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('8','2', 'Java RE', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('9', '3', 'Object is a addon for Java', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('10', '3', 'You have to use the program Object to run Java.', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('11', '3', 'Java can be extended with Objects.', true);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('12', '3', 'Java has no Objects', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('13', '4', 'Java S', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('14', '4', 'Java E', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('15', '4', 'Java SS', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('16', '4', 'Java SE', true);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('17', '5', 'Multiple Inheritance', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('18', '5', 'Classes&Methods', true);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('19', '5', 'Functions', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('20', '5', 'None of the above', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('21', '6', 'Classes', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('22', '6', 'only state', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('23', '6', 'only behaviour', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('24', '6', 'state and behaviour', true);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('25', '7', 'Global', true);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('26', '7', 'Local', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('27', '7', 'Class', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('28', '7', 'Instance', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('29', '8', 'Different name as Class', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('30', '8', 'Same name as attribute', false);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('31', '8', 'Same name as Class', true);
+INSERT INTO game.antwortmoeglichkeiten ("antwid", "fid", "antwort", "richtigeantwort") VALUES ('32', '8', 'Must contain attributes', false);
+
 INSERT INTO game.frage ("fid", "qid", "frage", "punktezahl") VALUES ('1', '1', 'What are advantages of Java?', 10);
 INSERT INTO game.frage ("fid", "qid", "frage", "punktezahl") VALUES ('2', '1', 'What is written after a line of code?', 10);
 INSERT INTO game.frage ("fid", "qid", "frage", "punktezahl") VALUES ('9', '1', 'What does Object Oriented mean?', 10);
